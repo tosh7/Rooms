304bb2baaaed481b3c83393ee39bdf38ed8ec6b0
tosh7 <zlia.6.lj.425@gmail.com>
2019-06-06 03:02:46 +0900

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Satoshi Komatsu on 2019/06/06.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
