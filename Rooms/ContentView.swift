f25a0eb2fd64e525b1678f4d2977e20eafa41b7f
tosh7 <zlia.6.lj.425@gmail.com>
2019-06-06 03:36:29 +0900

    6/6

--- Rooms/ContentView.swift
+++ Rooms/ContentView.swift
@@ -9,15 +9,26 @@
 import SwiftUI
 
 struct ContentView : View {
+    var rooms: [Room] = []
+    
     var body: some View {
-        Text("Hello World")
+        List(rooms) { room in
+            Image(systemName: "photo")
+            
+            VStack(alignment: .leading) {
+                Text(room.name)
+                Text("\(room.capacity) people")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rooms: testData)
     }
 }
 #endif
